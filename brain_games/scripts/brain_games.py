3c0de1419bea314c046a85dfd5b4d867d74101f5
Maksim Lesnikov <maximborzya75@gmail.com>
2022-10-26 15:27:57 +0600

    create module cli.py and func welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+from brain_games.scripts.cli import welcome_user
+
+
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
