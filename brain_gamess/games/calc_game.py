743c9df0c41ea68e2fdc607f583618ffb8c27ba6
Maksim Lesnikov <maximborzya75@gmail.com>
2022-10-31 18:13:54 +0600

    change func get_sign to func random.choice

--- brain_gamess/games/calc_game.py
+++ brain_gamess/games/calc_game.py
@@ -1,23 +1,18 @@
-from random import randint
+from random import randint, choice
 
 LOWER_LIMIT = 1
 UPPER_LIMIT = 100
 DESCRIPTION = 'What is the result of the expression?'
+SIGNS = ['+', '-', '*']
 
 
 def get_task():
     first_number = randint(LOWER_LIMIT, UPPER_LIMIT)
     second_number = randint(LOWER_LIMIT, UPPER_LIMIT)
-    sign = get_sign()
+    sign = choice(SIGNS)
     question = f'{first_number} {sign} {second_number}'
     correct_answer = str(get_result(first_number, sign, second_number))
     return question, correct_answer
-
-
-def get_sign():
-    signs = ['+', '-', '*']
-    sign = signs[randint(0, len(signs) - 1)]
-    return sign
 
 
 def get_result(first_num, sign, second_num):
